bf869238961b584f1c9943aefdff86b99beaa5a9
Sol1ked <aprima337228@gmail.com>
2024-02-09 21:04:02 +0700

    add selection sort

--- script.js
+++ script.js
@@ -23,5 +23,33 @@ function binarySearch(list, item) {
 
 let testList = [1, 2, 3, 4, 5, 6];
 
-console.log(binarySearch(testList, 2));
-console.log(binarySearch(testList, -1));
+// console.log(binarySearch(testList, 2));
+// console.log(binarySearch(testList, -1));
+
+//Selection Sort
+
+function findSmalles(arr) {
+  let smallest = arr[0];
+  let smallestIndex = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < smallest) {
+      smallest = arr[i];
+      smallestIndex = i;
+    }
+  }
+
+  return smallestIndex;
+}
+
+function selectionSort(arr) {
+  let newArr = [];
+
+  while (arr.length > 0) {
+    let smallest = findSmalles(arr);
+    newArr.push(arr.splice(smallest, 1)[0]);
+  }
+
+  return newArr;
+}
+
+console.log(selectionSort([5, 3, 6, 2, 19]));
